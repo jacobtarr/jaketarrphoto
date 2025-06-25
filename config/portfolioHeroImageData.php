ed0f777a67358d80d036602d88d46efa0f7fccca
Jacob Tarr <jtarr.rams@gmail.com>
2025-06-25 11:42:09 -0700

    partials

---
+++ config/portfolioHeroImageData.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+  '' => ['image' => '/dist/images/portfolio-hero/base.jpg', 'heading' => ''],
+  'arizona' => ['image' => '/dist/images/portfolio-hero/arizona.jpg', 'heading' => 'Arizona'],
+  'utah' => ['image' => '/dist/images/portfolio-hero/utah.jpg', 'heading' => 'Utah'],
+  'colorado' => ['image' => '/dist/images/portfolio-hero/colorado.jpg', 'heading' => 'Colorado'],
+  'new-mexico' => ['image' => '/dist/images/portfolio-hero/new-mexico.jpg', 'heading' => 'New Mexico'],
+  'nevada' => ['image' => '/dist/images/portfolio-hero/nevada.jpg', 'heading' => 'Nevada'],
+];
