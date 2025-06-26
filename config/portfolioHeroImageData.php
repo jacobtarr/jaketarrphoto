f2c648f90cd45982e5d39324be9d1f34751e1f7d
Jacob Tarr <jtarr.rams@gmail.com>
2025-06-25 19:27:12 -0700

    optimize hero

--- config/portfolioHeroImageData.php
+++ config/portfolioHeroImageData.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-  '' => ['image' => '/dist/images/portfolio-hero/base.jpg', 'heading' => ''],
-  'arizona' => ['image' => '/dist/images/portfolio-hero/arizona.jpg', 'heading' => 'Arizona'],
-  'utah' => ['image' => '/dist/images/portfolio-hero/utah.jpg', 'heading' => 'Utah'],
-  'colorado' => ['image' => '/dist/images/portfolio-hero/colorado.jpg', 'heading' => 'Colorado'],
-  'new-mexico' => ['image' => '/dist/images/portfolio-hero/new-mexico.jpg', 'heading' => 'New Mexico'],
-  'nevada' => ['image' => '/dist/images/portfolio-hero/nevada.jpg', 'heading' => 'Nevada'],
+  '' => ['image' => '/dist/images/portfolio-hero/base.webp', 'heading' => ''],
+  'arizona' => ['image' => '/dist/images/portfolio-hero/arizona.webp', 'heading' => 'Arizona'],
+  'utah' => ['image' => '/dist/images/portfolio-hero/utah.webp', 'heading' => 'Utah'],
+  'colorado' => ['image' => '/dist/images/portfolio-hero/colorado.webp', 'heading' => 'Colorado'],
+  'new-mexico' => ['image' => '/dist/images/portfolio-hero/new-mexico.webp', 'heading' => 'New Mexico'],
+  'nevada' => ['image' => '/dist/images/portfolio-hero/nevada.webp', 'heading' => 'Nevada'],
 ];
