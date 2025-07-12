e2586e193c85f1c26646142d5ce7a8b504cbcfb5
Jacob Tarr <jtarr.rams@gmail.com>
2025-07-12 12:18:43 -0700

    Add breadcrumbs and social bar

--- src/js/index.js
+++ src/js/index.js
@@ -28,16 +28,59 @@ function headerState() {
   };
 }
 
-function breadcrumbBar({ backTarget = '/', backText = 'Back', refMatch = backTarget } = {}) {
+function breadcrumbBar({
+  backTarget = '/',
+  backText = 'Back',
+  refMatch = backTarget,
+  swapTextOnScroll = false,
+  photoTitle = ''
+} = {}) {
   return {
     backHref: '',
     backText,
+    scrollPercent: 0,
+    showPhotoTitle: false,
+    hasInteracted: false,
+    photoTitle,
 
     init() {
       const referrer = document.referrer;
       const shouldUseHistory = referrer.includes(refMatch);
-
       this.backHref = shouldUseHistory ? 'javascript:history.back()' : backTarget;
+
+      this.updateScroll();
+      window.addEventListener('scroll', this.updateScroll.bind(this));
+
+      if (swapTextOnScroll) {
+        const titleEl = document.querySelector('[data-photo-title-trigger]');
+        if (titleEl) {
+          const observer = new IntersectionObserver(([entry]) => {
+            this.hasInteracted = true;
+            this.showPhotoTitle = !entry.isIntersecting;
+
+            // Add class to DOM to enable transitions
+            this.$el.classList.add('has-interacted');
+          }, { threshold: 0 });
+
+          observer.observe(titleEl);
+
+          requestAnimationFrame(() => {
+            const rect = titleEl.getBoundingClientRect();
+            if (rect.bottom < 0 || rect.top > window.innerHeight) {
+              this.hasInteracted = false;
+              this.showPhotoTitle = true;
+              this.$el.classList.remove('has-interacted');
+            }
+          });
+        }
+      }
+    },
+
+    updateScroll() {
+      const doc = document.documentElement;
+      const scrollTop = window.scrollY || doc.scrollTop;
+      const scrollHeight = doc.scrollHeight - doc.clientHeight;
+      this.scrollPercent = Math.min((scrollTop / scrollHeight) * 100, 100).toFixed(1);
     }
   };
 }
