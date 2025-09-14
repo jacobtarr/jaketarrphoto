676b9be73b8d82f871557551830eceba4f288bbb
Jacob Tarr <jtarr.rams@gmail.com>
2025-09-14 08:38:51 -0700

    restructure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 // CSS
 import './css/index.css';
 
+// Auto-import component + block CSS
+import.meta.glob(['../templates/_components/**/*.css', '../templates/_blocks/**/*.css'], { eager: true });
+
+// Auto-import component + block JS
+import.meta.glob(['../templates/_components/**/*.js', '../templates/_blocks/**/*.js'], { eager: true });
+
+
 // JS
 import './js/index.js';
