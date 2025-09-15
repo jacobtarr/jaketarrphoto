2e54a0ef4f45a16b1bf7d9768fcb0e7a9a02dd50
Jacob Tarr <jtarr.rams@gmail.com>
2025-09-15 08:10:08 -0700

    More restructuring

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './css/index.css';
-import.meta.glob(['../templates/_components/**/*.css', '../templates/_blocks/**/*.css'], { eager: true });
-import.meta.glob(['../templates/_components/**/*.js',  '../templates/_blocks/**/*.js' ], { eager: true });
+import.meta.glob(['../templates/_components/**/*.css', '../templates/_patterns/**/*.css', '../templates/_blocks/**/*.css'], { eager: true });
+import.meta.glob(['../templates/_components/**/*.js', '../templates/_patterns/**/*.js',  '../templates/_blocks/**/*.js' ], { eager: true });
 import './js/index.js'
