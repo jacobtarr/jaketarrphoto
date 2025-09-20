4018e9e99f277c12405be4d13b69b506a348ce6f
Jacob Tarr <jtarr.rams@gmail.com>
2025-09-19 22:01:32 -0700

    adjust headings

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import './css/index.css';
-import.meta.glob(['../templates/_components/**/*.css', '../templates/_patterns/**/*.css', '../templates/_blocks/**/*.css'], { eager: true });
-import.meta.glob(['../templates/_components/**/*.js', '../templates/_patterns/**/*.js',  '../templates/_blocks/**/*.js' ], { eager: true });
+
+import.meta.glob([
+  '../templates/_globals/**/*.css',
+  '../templates/_pages/**/*.css',
+  '../templates/_components/**/*.css',
+  '../templates/_patterns/**/*.css',
+  '../templates/_blocks/**/*.css'
+], { eager: true });
+
+import.meta.glob([
+  '../templates/_globals/**/*.js',
+  '../templates/_pages/**/*.js',
+  '../templates/_components/**/*.js',
+  '../templates/_patterns/**/*.js',
+  '../templates/_blocks/**/*.js'
+], { eager: true });
+
 import './js/index.js'
